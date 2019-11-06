8b15de87ee8ca00dd8f49ff8ca448a6d063e574e
Mohammad <mohammad8758@hotmail.com>
2019-11-07 02:25:48 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App" style={{ margin: "25px", background: "darkgrey" }}>
+      Tabs
+    </div>
+  );
+};
+
+export default App;
