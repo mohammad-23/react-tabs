31efdcbf2bc6517dd9ab010dc760898226879ba0
Mohammad <mohammad8758@hotmail.com>
2019-11-08 20:46:42 +0530

    Added the isDisabled Prop and fixed disabled tab css

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,12 @@ const App = () => {
         <Tabs.TabList>
           <Tabs.Tab isDisabled>First Tab</Tabs.Tab>
           <Tabs.Tab>Second Tab</Tabs.Tab>
+          <Tabs.Tab isActive>ThirdTab</Tabs.Tab>
         </Tabs.TabList>
         <Tabs.TabPanels>
           <Tabs.TabPanel>First TabPanel</Tabs.TabPanel>
           <Tabs.TabPanel>Second TabPanel</Tabs.TabPanel>
+          <Tabs.TabPanel>Third TabPanel</Tabs.TabPanel>
         </Tabs.TabPanels>
       </Tabs>
     </div>
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,17 +1,17 @@
 import "../styles/TabsStyles.css";
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import TabsContext from "../contexts/TabsContext";
 
 const Tab = props => {
   const { activeTab, setActiveTab } = useContext(TabsContext);
 
-  if (props.isActive) {
-    setActiveTab(props.index);
-  }
-
   const className = `tab ${
-    props.isDisabled ? "disabled" : activeTab === props.index ? "active" : null
+    props.isDisabled
+      ? "disabled inactive"
+      : activeTab === props.index
+      ? "active"
+      : ""
   }`;
 
   return (
@@ -19,6 +19,7 @@ const Tab = props => {
       className={className}
       onClick={() => {
         if (!props.isDisabled) {
+          console.log("dsd", props);
           setActiveTab(props.index);
         }
       }}
@@ -32,36 +33,58 @@ const TabList = props => {
   const { setActiveTab } = useContext(TabsContext);
 
   const children = React.Children.map(props.children, (child, index) => {
-    if (child.props.isDisabled && index === 0) {
-      setActiveTab(index++);
-    }
-
     return React.cloneElement(child, {
       index
     });
   });
 
+  useEffect(() => {
+    const activatedTab = children.find(item => item.props.isActive);
+    if (activatedTab) {
+      setActiveTab(activatedTab.props.index);
+    } else {
+      const firstTabNotDisabled = children.find(item => !item.props.isDisabled);
+      setActiveTab(firstTabNotDisabled.props.index);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return <div className="tabs-list">{children}</div>;
 };
 
 const TabPanel = props => {
-  const { activeTab } = useContext(TabsContext);
-
-  return <div>{props.index === activeTab ? props.children : null}</div>;
+  return <div>{props.children}</div>;
 };
 
 const TabPanels = props => {
+  const { activeTab } = useContext(TabsContext);
+
   const children = React.Children.map(props.children, (child, index) => {
     return React.cloneElement(child, {
       index
     });
   });
 
-  return <div>{children}</div>;
+  return <div>{children[activeTab]}</div>;
 };
 
 const Tabs = props => {
   const [activeTab, setActiveTab] = useState(0);
+
+  const disabledTabs = props.children[0].props.children.map(
+    item => item.props.isDisabled
+  );
+
+  const children = React.Children.map(props.children, (child, index) => {
+    if (child.type.name === "TabPanels") {
+      return React.cloneElement(child, {
+        disabledTabs
+      });
+    }
+
+    return child;
+  });
 
   return (
     <TabsContext.Provider
@@ -70,7 +93,7 @@ const Tabs = props => {
         setActiveTab
       }}
     >
-      {props.children}
+      {children}
     </TabsContext.Provider>
   );
 };
